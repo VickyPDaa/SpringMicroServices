5b5b1620af24741ff5463968eea23e69bfe67ab8
8936940055 <52422375+8936940055@users.noreply.github.com>
2024-10-11 13:20:38 +0530

    FirstSpringBoot Microservice project, loadbalanicing and communication between two services

---
+++ Eureka-Service-Registery/src/test/java/com/microserv/Eureka_Service_Registery/EurekaServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserv.Eureka_Service_Registery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
